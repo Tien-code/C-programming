3cc032bedbcb5105a7407384fe9f2da9606faefe
Tien-code <73330802+Tien-code@users.noreply.github.com>
2020-11-03 18:49:41 +0700

    code lap10

---
+++ vd2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+   incre();
+   incre(); 
+   incre();
+}
+
+   incre()
+   {
+   	char var =65;
+   	printf("\nThe character stored in var is %c", var++);
+   }
+   
+   
